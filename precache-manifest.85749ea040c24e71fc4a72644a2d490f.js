fa5a1d999031f39b2e2fa773942f10d5814c355b
Jack <40455730+idogmat@users.noreply.github.com>
2020-10-18 09:40:06 +0300

    Updates

---
+++ precache-manifest.85749ea040c24e71fc4a72644a2d490f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77027ec294a23ef3e9877be10ac7b84",
+    "url": "/idogmat/react-redux-todo/index.html"
+  },
+  {
+    "revision": "4cfc53d587c2250521e7",
+    "url": "/idogmat/react-redux-todo/static/css/main.2bf157d5.chunk.css"
+  },
+  {
+    "revision": "98cf4f1dc50e8f059078",
+    "url": "/idogmat/react-redux-todo/static/js/2.7ba29baa.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/idogmat/react-redux-todo/static/js/2.7ba29baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfc53d587c2250521e7",
+    "url": "/idogmat/react-redux-todo/static/js/main.e2be3457.chunk.js"
+  },
+  {
+    "revision": "d6054ee52e22dabb59ea",
+    "url": "/idogmat/react-redux-todo/static/js/runtime-main.73fe4bc4.js"
+  }
+]);
